34af4e279bbbf661d7405ad89cee7101d3a49b95
VladimirLogachev <logachev.dev@ya.ru>
2020-11-09 19:09:39 +0700

    Refactor an app, add mutations (#3)

--- src/Gql.elm
+++ src/Gql.elm
@@ -1,67 +1,61 @@
 module Gql exposing (..)
 
-import Graphql.Http
-import Graphql.Operation exposing (RootMutation, RootQuery)
-import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
-import LibraryApi.InputObject exposing (NewAuthor)
-import LibraryApi.Mutation as Mutation exposing (NewAuthorRequiredArguments)
-import LibraryApi.Object exposing (Author, Book)
-import LibraryApi.Object.Author as Author
-import LibraryApi.Object.Book as Book
-import LibraryApi.Query as Query exposing (AuthorsRequiredArguments, BooksRequiredArguments)
+import Dict
+import Graphql.Http as Http
+import Graphql.Http.GraphqlError
+import RemoteData exposing (RemoteData(..))
 import Task exposing (Task)
 
 
+{-| elm-graphql allows also to get some "possibly recovered data",
+but we don't care, that's why we have a Unit type as a parameter to Error.
+-}
+type alias GraphqlResponse a =
+    RemoteData (Http.Error ()) a
+
+
+type alias GraphqlTask t =
+    Task (Http.Error ()) t
+
+
+{-| GraphQL endpoint url.
+Ideally, in real apps, it should be passed to the App as a flag
+-}
 graphqlUrl : String
 graphqlUrl =
     "http://localhost:8000"
 
 
-type alias AuthorData =
-    { name : String }
+{-| Helper function for search queries.
+Though not expressed in graphql schema,
+the search string is actually expected to be a pattern for an SQL database
+(this design choice is less obvious for API users, but more flexible).
+"%" means "anything", so we search for a specified string with anything before and after it.
+-}
+toPattern : String -> String
+toPattern str =
+    "%" ++ str ++ "%"
 
 
-type alias GraphqlTask t =
-    Task (Graphql.Http.Error ()) t
+{-| Show error message from GraphqlResponse
+-}
+showError : Http.Error () -> String
+showError err =
+    case err of
+        Http.HttpError Http.NetworkError ->
+            "Network error"
 
+        Http.HttpError (Http.BadUrl _) ->
+            "BadUrl"
 
-authorSelection : SelectionSet AuthorData Author
-authorSelection =
-    SelectionSet.map AuthorData Author.name
+        Http.HttpError Http.Timeout ->
+            "Timeout"
 
+        Http.HttpError (Http.BadStatus _ _) ->
+            "BadStatus"
 
-findAuthors : String -> GraphqlTask (List AuthorData)
-findAuthors name =
-    Query.authors (AuthorsRequiredArguments name) authorSelection
-        |> Graphql.Http.queryRequest graphqlUrl
-        |> Graphql.Http.toTask
-        |> Task.mapError (Graphql.Http.mapError <| always ())
+        Http.HttpError (Http.BadPayload _) ->
+            "BadStatus"
 
-
-type alias BookData =
-    { title : String
-    , author : AuthorData
-    }
-
-
-bookSelection : SelectionSet BookData Book
-bookSelection =
-    SelectionSet.map2 BookData Book.title <| Book.author authorSelection
-
-
-findBooks : String -> GraphqlTask (List BookData)
-findBooks title =
-    Query.books (BooksRequiredArguments title) bookSelection
-        |> Graphql.Http.queryRequest graphqlUrl
-        |> Graphql.Http.toTask
-        |> Task.mapError (Graphql.Http.mapError <| always ())
-
-
-findAuthorsAndBooks : String -> GraphqlTask ( List AuthorData, List BookData )
-findAuthorsAndBooks queryStr =
-    findAuthors queryStr
-        |> Task.andThen
-            (\authors ->
-                findBooks queryStr
-                    |> Task.map (\books -> ( authors, books ))
-            )
+        Http.GraphqlError _ graphqlErrors ->
+            List.map (\e -> e.message) graphqlErrors |> String.join ", "
---
+++ src/Search.elm
@@ -0,0 +1,192 @@
+module Search exposing (Model, Msg(..), init, update, view)
+
+import Gql exposing (GraphqlResponse, GraphqlTask)
+import Graphql.Http
+import Graphql.Operation exposing (RootMutation, RootQuery)
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
+import Html exposing (Html, a, button, div, h1, input, p, span, text)
+import Html.Attributes exposing (class, href, placeholder, rel, target, type_, value)
+import Html.Events exposing (onClick, onInput)
+import LibraryApi.Object exposing (Author, Book)
+import LibraryApi.Object.Author as Author
+import LibraryApi.Object.Book as Book
+import LibraryApi.Query as Query exposing (AuthorsRequiredArguments, BooksRequiredArguments)
+import RemoteData
+import Task exposing (Task)
+
+
+type alias AuthorData =
+    { name : String
+    }
+
+
+type alias BookData =
+    { title : String
+    , authorName : String
+    }
+
+
+type alias SearchResults =
+    { authors : List AuthorData
+    , books : List BookData
+    }
+
+
+type alias Model =
+    { query : String
+    , searchResponse : GraphqlResponse SearchResults
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { query = ""
+      , searchResponse = RemoteData.NotAsked
+      }
+    , Cmd.none
+    )
+
+
+
+-- update and Tasks
+
+
+type Msg
+    = QueryChanged String
+    | GotSearchResponse (GraphqlResponse SearchResults)
+    | OpenEditorClicked
+
+
+{-| That's the simplest example of how we convert the response to domain types.
+-}
+authorSelection : SelectionSet AuthorData Author
+authorSelection =
+    SelectionSet.map AuthorData Author.name
+
+
+{-| This is a bit more complex example - we unnest response data to convert to our domain types
+Also, we can define one SelectionSet in terms of other SelectionSets.
+-}
+bookSelection : SelectionSet BookData Book
+bookSelection =
+    SelectionSet.map2 BookData
+        Book.title
+        (Book.author Author.name)
+
+
+findAuthors : String -> SelectionSet (List AuthorData) RootQuery
+findAuthors =
+    Gql.toPattern
+        >> AuthorsRequiredArguments
+        >> (\args -> Query.authors args authorSelection)
+
+
+findBooks : String -> SelectionSet (List BookData) RootQuery
+findBooks =
+    Gql.toPattern
+        >> BooksRequiredArguments
+        >> (\args -> Query.books args bookSelection)
+
+
+{-| That's how we can run 2 Queries in 1 HTTP request, combining their results to a SearchResults record.
+This SelectionSet can still become a part of something greater.
+-}
+findAuthorsAndBooks : String -> SelectionSet SearchResults RootQuery
+findAuthorsAndBooks queryStr =
+    SelectionSet.map2 SearchResults
+        (findAuthors queryStr)
+        (findBooks queryStr)
+
+
+findAuthorsAndBooksTask : String -> GraphqlTask SearchResults
+findAuthorsAndBooksTask =
+    findAuthorsAndBooks
+        >> Graphql.Http.queryRequest Gql.graphqlUrl
+        >> Graphql.Http.toTask
+        >> Task.mapError (Graphql.Http.mapError <| always ())
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        QueryChanged queryStr ->
+            case String.trim queryStr of
+                "" ->
+                    ( { model | query = "", searchResponse = RemoteData.NotAsked }, Cmd.none )
+
+                trimmedStr ->
+                    ( { model | query = queryStr, searchResponse = RemoteData.Loading }
+                    , findAuthorsAndBooksTask trimmedStr
+                        |> Task.attempt (RemoteData.fromResult >> GotSearchResponse)
+                    )
+
+        GotSearchResponse res ->
+            ( { model | searchResponse = res }, Cmd.none )
+
+        OpenEditorClicked ->
+            ( model, Cmd.none )
+
+
+
+-- views
+
+
+showAuthor : AuthorData -> Html Msg
+showAuthor { name } =
+    div [ class "fade-in" ] [ text name ]
+
+
+showBook : BookData -> Html Msg
+showBook { title, authorName } =
+    div [ class "book fade-in" ]
+        [ div [ class "book__cover" ] []
+        , div []
+            [ p [ class "book__title" ] [ text title ]
+            , p [ class "book__author" ] [ text <| "by " ++ authorName ]
+            ]
+        ]
+
+
+showSearchResults : SearchResults -> Html Msg
+showSearchResults { authors, books } =
+    let
+        items =
+            List.map showAuthor authors ++ List.map showBook books
+    in
+    case items of
+        [] ->
+            div [ class "no-results fade-in" ] [ text "Nothing found" ]
+
+        _ ->
+            div [ class "search-results fade-in" ] items
+
+
+showSearchResponse : GraphqlResponse SearchResults -> Html Msg
+showSearchResponse data =
+    case data of
+        RemoteData.NotAsked ->
+            div [] []
+
+        RemoteData.Loading ->
+            div [] [ p [ class "fade-in-slow" ] [ text ". . ." ] ]
+
+        RemoteData.Failure e ->
+            div [] [ p [ class "error fade-in" ] [ text (Gql.showError e) ] ]
+
+        RemoteData.Success results ->
+            showSearchResults results
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick OpenEditorClicked ] [ text "Add a book" ]
+        , input
+            [ type_ "text"
+            , placeholder "For example, Kant"
+            , value model.query
+            , onInput QueryChanged
+            ]
+            []
+        , showSearchResponse model.searchResponse
+        ]
