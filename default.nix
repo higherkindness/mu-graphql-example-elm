dac8c0c6a0ad18c77d17f3e58f8a35f97e069010
Flavio Corpa <flavio.corpa@47deg.com>
2020-11-18 22:25:48 +0100

    Add gitignore.nix ❄️

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
 let
+  pkgs = import <nixpkgs> {};
   nix-pre-commit-hooks = import (builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master");
+  gitignoreSrc = pkgs.fetchFromGitHub { 
+    owner = "hercules-ci";
+    repo = "gitignore";
+    rev = "c4662e6";
+    sha256 = "sha256:1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
 in {
   pre-commit-check = nix-pre-commit-hooks.run {
-    src = ./.;
+    src = gitignoreSource ./.;
     hooks = {
       elm-format.enable = true;
       ormolu.enable = true;
